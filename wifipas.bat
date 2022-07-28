57069e1d6bd6d4993fb5f920bca72f4fcb75b8b1
Karthik Chary <63688597+karthikchary12@users.noreply.github.com>
2022-07-28 11:59:35 +0530

    Update wifipas.bat
    2.0_V

--- wifipas.bat
+++ wifipas.bat
@@ -5,13 +5,13 @@ echo                                This script show the password for the Wifi
 echo ==================================================================================================================
 netsh wlan show profile 
 echo Enter the Name of the WIFI 
-echo (Note: If the Selected Wifi has white space between two strings mention it in " " Example: "Karthik Chary" )
 set /p input= Name of the WIFI = 
 echo Selected WIFI is: %input%
-netsh wlan show profile %input% key=clear | findstr "Key Content"
+netsh wlan show profile "%input%" key=clear | findstr "Key Content"
 
 pause
 set choice=
 set /p choice="Do you want to check one more password? Press 'y' or 'n' to continue : "
 if not '%choice%'=='' set choice=%choice:~0,1%
 if '%choice%'=='y' goto start 
+
